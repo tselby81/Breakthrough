03028ba8eb5106e97582b18c790adfc25d56edb4
tselby81 <106200743+tselby81@users.noreply.github.com>
2023-04-26 22:52:29 -0400

    update main

--- brkthrgh_extended_bonus.py
+++ brkthrgh_extended_bonus.py
@@ -3,6 +3,3 @@
 """
 Game file if we want to create Breakthrough gamemode with extended ruleset for BONUS POINTS.
 """
-Long Rectangular Board: For this variation, stick to the original, 1-worker-toenemy-base win criterion. Instead, change the board shape to an oblong
-rectangle of dimensions 5x10.
-"""
