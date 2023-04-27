c9baaa88d7c7f4410601b4e5e82408f780c53291
tselby81 <106200743+tselby81@users.noreply.github.com>
2023-04-26 22:57:35 -0400

    update

--- alpha_beta_agent.py
+++ alpha_beta_agent.py
@@ -1,4 +1,14 @@
-from model import *
+
+
+"""
+This will hold some of the logic that will be used by the minimax and alpha-beta search agents.
+Including the board matrix, piece-movement logic (available moves, capturing pieces, check-turn), alternating turns, 
+win-conditions, offensive functions(agent focuses on moving forward and capturing opponent's pieces), 
+and defensive functions(agent focuses on defending territory and preventing opponent from capturing pieces.)
+"""
+
+from logic import *
+
 
 class AlphaBetaAgent:
     def __init__(self, boardmatrix, turn, depth, function, type=0):
@@ -14,10 +24,13 @@ class AlphaBetaAgent:
     def max_value(self, state, alpha, beta, depth):
         if depth == self.maxdepth or state.isgoalstate() != 0:
             return state.utility(self.turn)
-        v = MINNUM
-        actions = state.available_actions()
+        v = MINVAL
+        actions = state.check_possible_actions()
 
-        actions = sorted(state.available_actions(), key=lambda action: self.orderaction(action, state), reverse=True)
+        #if self.turn == 1:
+        actions = sorted(state.check_possible_actions(), key=lambda action: self.orderaction(action, state), reverse=True)
+        #else:
+        #    actions = sorted(state.possible_actions(), key=lambda action: self.orderaction(action, state))
 
         for action in actions:
             self.nodes += 1
@@ -31,10 +44,13 @@ class AlphaBetaAgent:
     def min_value(self, state, alpha, beta, depth):
         if depth == self.maxdepth or state.isgoalstate() != 0:
             return state.utility(self.turn)
-        v = MAXNUM
-        actions = state.available_actions()
+        v = MAXVAL
+        actions = state.check_possible_actions()
 
-        actions = sorted(state.available_actions(), key=lambda action: self.orderaction(action, state))
+        #if self.turn == 1:
+        actions = sorted(state.check_possible_actions(), key=lambda action: self.orderaction(action, state))
+        #else:
+        #    actions = sorted(state.check_possible_actions(), key=lambda action: self.orderaction(action, state), reverse=True)
 
         for action in actions:
             self.nodes += 1
@@ -51,23 +67,18 @@ class AlphaBetaAgent:
             initialstate = State(boardmatrix=self.boardmatrix, turn=self.turn, function=self.function)
         else:
             initialstate = State(boardmatrix=self.boardmatrix, turn=self.turn, function=self.function, height=5, width=10)
-        v = MINNUM
-        alpha = MINNUM
-        beta = MAXNUM
-        for action in initialstate.available_actions():
+        v = MINVAL
+        for action in initialstate.check_possible_actions():
             self.nodes += 1
 
             new_state = initialstate.transfer(action)
             if new_state.isgoalstate():
                 final_action = action
                 break
-            minresult = self.min_value(new_state, alpha, beta, 1)
+            minresult = self.min_value(new_state, MINVAL, MAXVAL, 1)
             if minresult > v:
                 final_action = action
                 v = minresult
-            alpha = max(alpha, v - 1)
-            if alpha >= beta:
-                break
         print(v)
         if self.turn == 1:
             self.piece_num = initialstate.transfer(final_action).white_num
@@ -78,6 +89,7 @@ class AlphaBetaAgent:
 
     # order actions to make more pruning
     def orderaction(self, action, state):
+
         y = action.coordinate[0]
         x = action.coordinate[1]
         if action.turn == 1:
@@ -87,7 +99,7 @@ class AlphaBetaAgent:
             if action.direction == 2:
                 if (y - 1, x) in state.white_positions:
                     return 2
-            if action.direction == 3:
+            if action.direction == 2:
                 if (y - 1, x + 1) in state.white_positions:
                     return 2
 
@@ -98,12 +110,16 @@ class AlphaBetaAgent:
             if action.direction == 2:
                 if (y + 1, x) in state.black_positions:
                     return 2
-            if action.direction == 3:
+            if action.direction == 2:
                 if (y + 1, x + 1) in state.black_positions:
                     return 2
+        return 1
+            #if action.coordinate[]
 
-        if action.turn == 1:
-            return max(state.get_farthest_piece(self.turn), action.coordinate[0] + 1)
-        elif action.turn == 2:
-            return max(state.get_farthest_piece(self.turn), 7 - action.coordinate[0] + 1)
+        #print(self.turn)
+        # return state.transfer(action).utility(self.turn)
+        #if action.turn == 1:
+        #    return max(state.get_farthest_piece(self.turn), action.coordinate[0] + 1)
+        #elif action.turn == 2:
+        #    return max(state.get_farthest_piece(self.turn), 7 - action.coordinate[0] + 1)
         return 0
